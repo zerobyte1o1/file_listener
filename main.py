5695c03f68a118d9107549c30a422ef1d04f5e0c
刘方靖 <liufangjing@shifang.co>
2024-01-12 20:07:36 +0800

    init

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import os
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
 class MyHandler(FileSystemEventHandler):
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.last_modified_time = os.path.getmtime(filepath)
+
     def on_modified(self, event):
         if not event.is_directory:
-            # 文件被修改时触发
-            print(f"File modified: {event.src_path}")
+            file_modified_time = os.path.getmtime(self.filepath)
+            if file_modified_time > self.last_modified_time:
+                self.last_modified_time = file_modified_time
+                # 文件被修改时触发
+                print(f"File modified: {event.src_path}")
 
 
 if __name__ == "__main__":
-    path = "c:/dish.listen"  # 监听的文件路径
+    path = "/path/to/file"  # 监听的文件路径
 
-    event_handler = MyHandler()
+    event_handler = MyHandler(path)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=False)
     observer.start()
