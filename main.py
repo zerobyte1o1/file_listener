519cd4f35b114053555f8bea9d1226a43d9cc1dc
刘方靖 <liufangjing@shifang.co>
2024-01-13 11:33:47 +0800

    init

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import os
 import time
 
 file_path = 'c:/dish.listen'
-last_modified = None
 
+try:
+    last_modified = os.path.getmtime(file_path)
+
+except Exception as e:
+    print(f'Error occurred: {e}')
+    
 while True:
     try:
         current_modified = os.path.getmtime(file_path)
         print(current_modified)
-        if last_modified is None or current_modified > last_modified:
+        if current_modified > last_modified:
             print('文件被修改')
             last_modified = current_modified
             os.system("""C:/"Program Files"/AIDish/Client/yx-dish.exe""")
