b5f1ecd865c8e9931e3e0902cb5f751cd7b07621
刘方靖 <liufangjing@shifang.co>
2024-01-13 10:52:50 +0800

    init

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
+import os
 import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
+file_path = '/Users/liufangjing/Downloads/mit2meter.json'
+last_modified = None
 
-class FileEventHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if not event.is_directory:
-            # 文件被修改时触发
-            print(f"File modified: {event.src_path}")
+while True:
+    current_modified = os.path.getmtime(file_path)
+    print(current_modified)
+    if last_modified is None or current_modified > last_modified:
+        print('文件被修改')
+        last_modified = current_modified
 
-    def on_moved(self, event):
-        if not event.is_directory and event.dest_path == r"c:\dish.listen":
-            # 文件被移动或重命名时触发，相当于文件被替换
-            print(f"File replaced: {event.src_path}")
-
-
-
-if __name__ == "__main__":
-    # 实例化Observer对象
-    observer = Observer()
-    event_handler = FileEventHandler()
-    # 设置监听目录
-    dis_dir = "c:/dish.listen"
-    observer.schedule(event_handler, dis_dir, True)
-    observer.start()
-    try:
-        while True:
-            # 设置监听频率(间隔周期时间)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
-    observer.join()
+    time.sleep(3)
