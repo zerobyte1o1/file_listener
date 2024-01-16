0671b14bdbe2926cdcdd07ee0bcac4c8cdf2435d
刘方靖 <liufangjing@shifang.co>
2024-01-16 16:07:04 +0800

    init

--- dish_listener.py
+++ dish_listener.py
@@ -45,7 +45,7 @@ while True:
             try:
                 print("拉取代码")
                 subprocess.Popen(
-                    'cmd /c "cd /d C:\\ && git clone https://liufj:lfj19980123@git.shifang.co/test/winuitest-AIDish.git && cd winuitest-AIDish && run.bat"')
+                    'cmd /c "cd /d C:\\ && git clone https://liufj:lfj19980123@git.shifang.co/test/winuitest-AIDish.git && cd winuitest-AIDish && run.bat"',timeout=5)
             except Exception as e:
                 print(f'Error occurred while cloning repository: {e}')
 
