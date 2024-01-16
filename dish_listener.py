9b0b9b62e0871d0877bf2013eb05e91a0b4db6e7
刘方靖 <liufangjing@shifang.co>
2024-01-16 17:16:58 +0800

    init

--- dish_listener.py
+++ dish_listener.py
@@ -14,42 +14,47 @@ except Exception as e:
 
 while True:
     try:
-        current_modified = os.path.getmtime(file_path)
-        print(current_modified)
-        if last_modified is None:
-            break
-        if current_modified > last_modified:
-            print('文件被修改')
-            last_modified = current_modified
-            if os.path.exists(auto_folder_path):
-                try:
-                    # Get the process ID using the port
-                    netstat_output = subprocess.check_output(f'netstat -ano | findstr :{port}', shell=True)
-                    pid = netstat_output.decode().split()[-1]
-                    print(f'Port {port} is in use')
-                    # Forcefully terminate the process using the port
-                    subprocess.check_output(f'taskkill /F /PID {pid}', shell=True)
-                    print(f'Process using port {port} terminated')
-                    # Delete the folder
-                except subprocess.CalledProcessError:
-                    print(f'Port {port} is not in use')
-
-                try:
-                    os.system(f'rd /s /q {auto_folder_path}')
-                    print(f'Folder {auto_folder_path} deleted')
-                except Exception as e:
-                    print(f'Error occurred while deleting folder: {e}')
-
-            else:
-                print(f'Folder {auto_folder_path} does not exist')
-            try:
-                print("拉取代码")
-                subprocess.Popen(
-                    'cmd /c "cd /d C:\\ && git clone https://liufj:lfj19980123@git.shifang.co/test/winuitest-AIDish.git && cd winuitest-AIDish && run.bat"',shell=True)
-            except Exception as e:
-                print(f'Error occurred while cloning repository: {e}')
-
+        current_modified = os.path.getmtime(file_path)  
     except Exception as e:
         print(f'Error occurred: {e}')
+        continue
+    print(current_modified)
+    if last_modified is None:
+        break
+    if current_modified > last_modified:
+        print('文件被修改')
+        last_modified = current_modified
+        if os.path.exists(auto_folder_path):
+            try:
+                # Get the process ID using the port
+                netstat_output = subprocess.check_output(f'netstat -ano | findstr :{port}', shell=True)
+                pid = netstat_output.decode().split()[-1]
+                print(f'Port {port} is in use')
+                # Forcefully terminate the process using the port
+                subprocess.check_output(f'taskkill /F /PID {pid}', shell=True)
+                print(f'Process using port {port} terminated')
+                # Delete the folder
+            except subprocess.CalledProcessError:
+                print(f'Port {port} is not in use')
+                continue
+
+            try:
+                os.system(f'rd /s /q {auto_folder_path}')
+                print(f'Folder {auto_folder_path} deleted')
+            except Exception as e:
+                print(f'Error occurred while deleting folder: {e}')
+                continue
+
+        else:
+            print(f'Folder {auto_folder_path} does not exist')
+        try:
+            print("拉取代码")
+            subprocess.Popen(
+                'cmd /c "cd /d C:\\ && git clone https://liufj:lfj19980123@git.shifang.co/test/winuitest-AIDish.git && cd winuitest-AIDish && run.bat"',shell=True)
+        except Exception as e:
+            print(f'Error occurred while cloning repository: {e}')
+            continue
+
+
 
     time.sleep(5)
